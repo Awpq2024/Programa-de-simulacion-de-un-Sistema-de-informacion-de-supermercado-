cb785aa743f3dac40701cfd221d91a259b02c5f8
Awpq2024 <awpariq@unjbg.edu.pe>
2024-07-15 22:52:39 -0500

    Se agrega el modelo del proyecto sistema de supermercado

---
+++ funciones.h
@@ -0,0 +1,9 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+#include <iostream>
+#include "producto.h"
+
+void agregarProducto(producto articulo[], int& numeroProducto, int& siguienteID);
+void mostrarProductos(const producto articulo[], int numeroProducto);
+
+#endif
