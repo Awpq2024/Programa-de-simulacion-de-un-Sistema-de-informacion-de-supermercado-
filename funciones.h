ca5bdaacae826e210c6f67547ad809872c9c9bac
Awpq2024 <awpariq@unjbg.edu.pe>
2024-07-20 21:01:45 -0500

    Se agrega un menu de reclamos, con la opcion de agregar, eliminar y mostrar los reclamos, siendo estos independientes de los productos y ventas

--- funciones.h
+++ funciones.h
@@ -3,10 +3,22 @@
 #include <iostream>
 #include "producto.h"
 
+//Funciones de productos
 void agregarProducto(producto articulo[], int& numeroProducto, int& siguienteID);
 void mostrarProductos(const producto articulo[], int numeroProducto);
+
+
+//funciones Ventas
 void registrarVenta(gestionDeventas ventas[], int& numeroVenta, producto articulo[], int& numeroProducto);
 void mostrarVentas(const gestionDeventas ventas[], int numeroVenta);
 void mostrarGanancias(gestionDeventas ventas[], int& numeroVenta);
+
+
+//Funciones reclamos
+void MenuReclamos(reclamo quejas[], int &numeroReclamo, int &siguienteIDReclamo);
+//--- Menu de Reclamos ---//
+void agregarReclamo(reclamo quejas[], int &numeroReclamo, int &siguienteIDReclamo);//Funcion Agregar un Reclamo
+void mostrarReclamos(const reclamo quejas[], int numeroReclamo);//Funcion Mostrar un Reclamo
+void eliminarReclamo(reclamo quejas[], int &numeroReclamo);//Funcion Eliminar un Reclamo
 
 #endif
